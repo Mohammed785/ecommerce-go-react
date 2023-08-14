829032016e8d50aabefe1c846d9bf8549017ea7c
Mohamed <mohammednady128@gmail.com>
2023-08-15 02:29:20 +0300

    add cors support to server

--- server/main.go
+++ server/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/Mohammed785/ecommerce/initializers"
 	"github.com/Mohammed785/ecommerce/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -19,10 +21,20 @@ func init(){
 
 func main(){
 	server := gin.Default()
+	server.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:5173"},
+		AllowMethods: []string{"POST","PUT","GET","DELETE"},
+		AllowHeaders: []string{"Origin","Content-type","Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "Accept", "Cache-Control", "X-Requested-With"},
+		ExposeHeaders: []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge: 12*time.Hour,
+	}))
 	server.GET("/",func(ctx *gin.Context) {
 		ctx.JSON(200,gin.H{})
 	})
 	authRouter := server.Group("/api/v1/auth");
-	routes.SetupUserRoute(authRouter)
+	userRouter := server.Group("/api/v1/user");
+	routes.SetupAuthRoute(authRouter)
+	routes.SetupUserRoute(userRouter)
 	server.Run();
 }
