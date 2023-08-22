9143e98741b343b85b481c74ac98045b3cc874a0
Mohamed <mohammednady128@gmail.com>
2023-08-22 17:42:56 +0300

    create product models & repository

--- server/models/product.go
+++ server/models/product.go
@@ -2,19 +2,24 @@ package models
 
 import "time"
 
+type ProductDetails struct{
+	Product Product
+	Category Category
+	Attributes []Attribute
+}
 
 type Product struct{
-	Id int `db:"id"`
-	Name string `db:"name"`
-	Description string `db:"description"`
-	Price float64 `db:"price"`
-	Stock int `db:"stock"`
-	Sku string `db:"sku"`
-	CreatedAt *time.Time `db:"crated_at"`
-	DeletedAt *time.Time `db:"deleted_at"`
-	CategoryId int `db:"category_id"`
-	Category *Category `db:"cat"`
-	Attributes *ProductAttributes `db:"attrs"`
+	Id int `db:"id" json:"id"`
+	Name string `db:"name" json:"name"`
+	Description *string `db:"description" json:"description"`
+	Price float64 `db:"price" json:"price"`
+	Stock int `db:"stock" json:"stock"`
+	Sku string `db:"sku" json:"sku"`
+	CreatedAt *time.Time `db:"created_at" json:"created_at"`
+	DeletedAt *time.Time `db:"deleted_at" json:"deleted_at"`
+	CategoryId *int `db:"category_id" json:"category_id"`
+	CategoryName *string `db:"category_name" json:"category_name"`
+	Attributes []ProductAttributes `json:"attributes"`
 }
 
 type Category struct{
@@ -29,12 +34,12 @@ type Attribute struct{
 }
 
 type ProductAttributes struct{
-	Value string `db:"value"`
-	Attribute *Attribute
+	Id int `db:"id" json:"id"`
+	Value string `db:"value" json:"value"`
+	AttributeType string `db:"attribute_type" json:"attribute_type"`
 }
 
 type ProductAttribute struct{
 	AttributeId int `json:"attribute_id" db:"attribute_id" binding:"required"`
-	ProductId int `json:"product_id" db:"product_id" binding:"required"`
 	Value string `json:"value" binding:"required,max=255"`
 }
---
+++ server/repository/product.go
@@ -0,0 +1,89 @@
+package repository
+
+import (
+	"github.com/Mohammed785/ecommerce/globals"
+	"github.com/Mohammed785/ecommerce/models"
+	"github.com/doug-martin/goqu/v9"
+	"github.com/jmoiron/sqlx"
+)
+
+
+type productRepository struct {}
+
+var ProductRepository *productRepository = &productRepository{}
+
+func (p *productRepository) FindOne(identifier string)(product models.Product,err error) {
+	err= globals.DB.Get(&product,`SELECT product.*,cat.name AS category_name FROM tbl_product product
+LEFT JOIN tbl_category cat ON cat.id=product.category_id
+WHERE (product.id=$1 OR product.sku=CAST($1 AS VARCHAR)) AND deleted_at IS NULL`,identifier)
+	if err!=nil{
+		return product,err
+	}
+	err = globals.DB.Select(&product.Attributes,"SELECT attr.id,pa.value,attr.attribute_type FROM tbl_product_attribute pa JOIN tbl_attribute attr ON attr.id=pa.attribute_id WHERE product_id=$1",product.Id)
+	return product,err
+}
+
+func (p *productRepository) Create(product interface{},attributes []models.ProductAttribute) error{
+	sql,_,_ := globals.Dialect.Insert("tbl_product").Rows(product).Returning("id").ToSQL()
+	row:= struct{ Id int64 }{Id:-1}
+	tx,err:=globals.DB.Beginx();
+	if err!=nil{
+		return err
+	}
+	res := tx.QueryRowx(sql)
+
+	err=res.StructScan(&row)
+	if err!=nil{
+		tx.Rollback()
+		return err
+	}
+	for _,attr:= range attributes{
+		_,err = tx.Exec("INSERT INTO tbl_product_attribute(product_id,attribute_id,value) VALUES($1,$2,$3)",row.Id,attr.AttributeId,attr.Value)
+		if err!=nil{
+			tx.Rollback()
+			return err
+		}
+	}
+	err = tx.Commit()
+	return err
+}
+
+func (p *productRepository) Update(id string,data interface{}) (int64,error){
+	sql,_,_:=globals.Dialect.Update("tbl_product").Where(goqu.C("id").Eq(id)).Where(goqu.C("deleted_at").Eq(nil)).Set(data).ToSQL()
+	result,err:=globals.DB.Exec(sql)
+	if err!=nil{
+		return 0,err
+	}
+	rows,err:=result.RowsAffected()
+	return rows,err
+}
+
+func (p *productRepository) Delete(id string,hardDelete bool) (int64,error){
+	var sql string;
+	if hardDelete{
+		sql = "DELETE FROM tbl_product WHERE id=$1"
+	}else{
+		sql ="UPDATE tbl_product SET deleted_at=now() WHERE id=$1 AND deleted_at IS NULL" 
+	}
+	result,err:=globals.DB.Exec(sql,id);
+	if err!=nil{
+		return 0,err
+	}
+	rows,err:=result.RowsAffected();
+	return rows,err
+}
+
+func (p *productRepository) AddAttributes(id int,attributes... interface{})error{
+	sql,_,_ := globals.Dialect.Insert("tbl_product_attribute").Rows(attributes...).ToSQL();
+	_,err:=globals.DB.Exec(sql)
+	return err
+}
+func (p *productRepository) RemoveAttribute(id string,attributes []int)error{
+	query,args,err:=sqlx.In("DELETE FROM tbl_product_attribute WHERE product_id=? AND attribute_id IN (?)", id,attributes)
+	if err!=nil{
+		return err
+	}
+	query = globals.DB.Rebind(query)
+	_,err=globals.DB.Exec(query,args...)
+	return err
+}
